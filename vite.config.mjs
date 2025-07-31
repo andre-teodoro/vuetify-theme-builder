8861516d5d6eb42fea11bdcfd2d88c68803dea77
andre-teodoro <130784630+andre-teodoro@users.noreply.github.com>
2025-07-31 16:36:51 -0300

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -14,6 +14,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE || '/',
   plugins: [
     VueRouter(),
     Pages(),
